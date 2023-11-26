82d34745307dab6916cce060d71ea0998c9d6750
walfredcutaran <100104566+walfredcutaran@users.noreply.github.com>
2023-11-27 00:08:08 +0800

    dev

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SpeechToText from './components/RecognitionTranscription';
 function App() {
   return (
     <div className="App">
+      
       <ParticlesComponent  />
       <SpeechToText />
     </div>
--- src/components/RecognitionTranscription.js
+++ src/components/RecognitionTranscription.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import styled from 'styled-components';
+import { FaBeer } from "react-icons/fa";
 
 const SpeechToText = () => {
 const {
@@ -20,7 +22,62 @@ return (
     <button onClick={SpeechRecognition.stopListening}>Stop</button>
     <button onClick={resetTranscript}>Reset</button>
     <p style={{color : 'white'}}>{transcript}</p>
+
+        <h3 style={{color: 'white'}}>
+            Lets go for a <FaBeer color='white'/>?
+        </h3>
+
+        <BaseRectangle>
+            <RecordButton style={{ top: 20, left: 20, width: 100, height: 80 }}>Record</RecordButton>
+            <PlayButton style={{ top: 20, right: 310, width: 100, height: 80 }}>Play</PlayButton>
+        </BaseRectangle>
+
     </div>
 );
 };
+
+const Button = styled.button`
+    position: absolute;
+    width: 50px;
+    height: 30px;
+    background-color: #007bff;
+    border-radius: 10px;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+`;
+
+const RecordButton = styled(Button)`
+    background-color: red
+`;
+
+const PlayButton = styled(Button)`
+    background-color: green
+`;
+
+// https://www.twilio.com/blog/audio-visualisation-web-audio-api--react
+const AudioVisualizer = styled.button`
+    position: absolute;
+    width: 50px;
+    height: 30px;
+    background-color: #007bff;
+    border-radius: 10px;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+`;
+
+const BaseRectangle = styled.div`
+    width: 550px;
+    height: 450px;
+    background-color: #2e2e2e;
+    border-radius: 20px;
+    position: absolute;
+    top: 60%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+
 export default SpeechToText;
+
+// Extend the Styles and optimize
