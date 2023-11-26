16bb5b6a9dcdc32d3c10e24a82271498684a642e
walfredcutaran <100104566+walfredcutaran@users.noreply.github.com>
2023-11-26 19:15:46 +0800

    2nd push, core files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ParticlesComponent from './components/Particles';
+import SpeechToText from './components/RecognitionTranscription';
+
+function App() {
+  return (
+    <div className="App">
+      <ParticlesComponent  />
+      <SpeechToText />
+    </div>
+  );
+}
+
+export default App;
