1f44e2c766d884284e83350f79bbfb07fa35697d
walfredcutaran <100104566+walfredcutaran@users.noreply.github.com>
2023-11-27 19:57:51 +0800

    dev

--- src/components/RecognitionTranscription.js
+++ src/components/RecognitionTranscription.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { AudioVisualizer, LiveAudioVisualizer } from 'react-audio-visualize';
 import styled from 'styled-components';
 import { FaBeer } from "react-icons/fa";
 
@@ -30,6 +31,7 @@ return (
         <BaseRectangle>
             <RecordButton style={{ top: 20, left: 20, width: 100, height: 80 }}>Record</RecordButton>
             <PlayButton style={{ top: 20, right: 310, width: 100, height: 80 }}>Play</PlayButton>
+            <TextArea style={{ height: 300, width: 510, fontSize: 20 }} spellCheck="false" value={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}/>
         </BaseRectangle>
 
     </div>
@@ -56,18 +58,20 @@ const PlayButton = styled(Button)`
 `;
 
 // https://www.twilio.com/blog/audio-visualisation-web-audio-api--react
-const AudioVisualizer = styled.button`
-    position: absolute;
-    width: 50px;
-    height: 30px;
-    background-color: #007bff;
-    border-radius: 10px;
-    color: #fff;
-    border: none;
-    cursor: pointer;
-`;
+// const AudioVisualizer = styled.button`
+//     position: absolute;
+//     width: 50px;
+//     height: 30px;
+//     background-color: #007bff;
+//     border-radius: 10px;
+//     color: #fff;
+//     border: none;
+//     cursor: pointer;
+// `;
 
 const BaseRectangle = styled.div`
+    display: flex;
+    justify-content: center;
     width: 550px;
     height: 450px;
     background-color: #2e2e2e;
@@ -76,6 +80,19 @@ const BaseRectangle = styled.div`
     top: 60%;
     left: 50%;
     transform: translate(-50%, -50%);
+`;
+
+const TextArea = styled.textarea`
+    position: absolute;
+    resize: none;
+    top: 60%;
+    transform: translateY(-50%);
+    flex: 0.7;
+    border-radius: 10px;
+    color: white;
+    background-color: black;
+    font-weight: 600;
+    border-color: black;
 `;
 
 export default SpeechToText;
