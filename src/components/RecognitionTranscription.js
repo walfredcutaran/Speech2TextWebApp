b8d61ecfc79b4624ebde59e35bc4843ca5f3cd83
walfredcutaran <100104566+walfredcutaran@users.noreply.github.com>
2024-01-08 21:31:46 +0800

    testing

--- src/components/RecognitionTranscription.js
+++ src/components/RecognitionTranscription.js
@@ -1,169 +1,237 @@
-import React , { useState, useEffect, useRef } from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import styled from 'styled-components';
-import { animated } from '@react-spring/web'
-import { useSpring } from '@react-spring/web'
-import {color, motion} from 'framer-motion';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState, useEffect, useRef } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import styled from "styled-components";
+import { animated } from "@react-spring/web";
+import { useSpring } from "@react-spring/web";
+import { color, motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import AudioMotionAnalyzer from "https://cdn.skypack.dev/audiomotion-analyzer?min";
-
-
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
 
 const SpeechToText = () => {
-const {
+  const {
     transcript,
     listening,
     resetTranscript,
     browserSupportsSpeechRecognition,
-    browserSupportsContinuousListening
-} = useSpeechRecognition();
+    browserSupportsContinuousListening,
+  } = useSpeechRecognition();
 
-  const [icon, setIcon] = useState('RecordStop');
-  const [show, setShow] = useState(false)
+  const [icon, setIcon] = useState("RecordStop");
+  const [show, setShow] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClickButton = () => {
-    setIcon(icon === 'RecordStop' ? 'eye-off' : 'RecordStop'); // Toggle icon state
+    setIcon(icon === "RecordStop" ? "eye-off" : "RecordStop"); // Toggle icon state
   };
 
+  if (!browserSupportsSpeechRecognition) {
+    alert("This Browser doesn't support speech recognition.");
+    return (
+      <span style={{ color: "red" }}>
+        This Browser doesn't support speech recognition.
+      </span>
+    );
+  }
 
-if (!browserSupportsSpeechRecognition) {
-    alert("This Browser doesn't support speech recognition.")
-    return <span style={{color: 'red'}}>This Browser doesn't support speech recognition.</span>;
-}
+  // if (browserSupportsContinuousListening) {
+  //   SpeechRecognition.startListening({ continuous: true })
+  // } else {
+  //   alert("This Browser doesn't support continues listening.")
+  //   return <span style={{color: 'red'}}>This Browser doesn't support continues listeing.</span>;
+  // }
 
-// if (browserSupportsContinuousListening) {
-//   SpeechRecognition.startListening({ continuous: true })
-// } else {
-//   alert("This Browser doesn't support continues listening.")
-//   return <span style={{color: 'red'}}>This Browser doesn't support continues listeing.</span>;
-// }
-
-return (
-  
+  return (
     <div>
-    <p style={{color : 'white'}}> Microphone: {listening ? 'on' : 'off'} </p>
+      <p style={{ color: "white" }}> Microphone: {listening ? "on" : "off"} </p>
 
-        <h3 style={{color: 'white'}}>
-            {/* Lets go for a <FaBeer color='white'/>? */}
-        </h3>
+      <h3 style={{ color: "white" }}>
+        {/* Lets go for a <FaBeer color='white'/>? */}
+      </h3>
 
-        <div className='framer-box'>
-          <motion.div
-            layout
-            transition={{ layout: { duration: .5, ease: 'easeOut' } }}
-            onClick={(event) => {event.stopPropagation(); setIsOpen(true);}}
-            className='card'
-          >
-
-            {isOpen ? (
-              
-              // <motion.div className='expand'>
-              <div className="expand">
+      <div className="framer-box">
+        <motion.div
+          layout
+          transition={{ layout: { duration: 0.5, ease: "easeOut" } }}
+          onClick={(event) => {
+            event.stopPropagation();
+            setIsOpen(true);
+          }}
+          className="card"
+        >
+          {isOpen ? (
+            // <motion.div className='expand'>
+            <div className="expand">
               {[SpeechRecognition.startListening, handleClickButton]}
-                <div className='button-area'>
-                
-                <PlayButton onClick={ SpeechRecognition.startListening} style={{ width: 100, height: 80, }} > <i class="animated-icon-start fa-solid fa-play fa-2xl"></i></PlayButton>
-                <RecordButton onClick={SpeechRecognition.stopListening} style={{width: 100, height: 80, 'margin-left': 110, } } > Stop </RecordButton>
-                <ResetButton onClick={resetTranscript} style={{width: 100, height: 80, 'margin-left': 450}}> <i class="animated-icon-reset fa-solid fa-arrow-rotate-left fa-2xl"></i> </ResetButton>
-                <RetractButton onClick={(event) => {event.stopPropagation(); setIsOpen(false);}} style={{width: 80, height: 60, 'margin-left': 350}}> Minimize </RetractButton>
-                {/* <RecordButton onClick={handleClickButton} style={{width: 100, height: 80, 'margin-left': 110}} > {icon === 'RecordStop' ? <FaCircleDot style={{fontSize: 50}} /> : <FaStopCircle style={{fontSize: 50}} />} </RecordButton> */}
-                </div>
+              <div className="button-area">
+                <PlayButton
+                  onClick={SpeechRecognition.startListening}
+                  style={{ width: 60, height: 60 }}
+                  id="startButton"
+                >
+                  {" "}
+                  <i class="animated-icon-start fa-solid fa-play fa-2xl"></i>
+                </PlayButton>
+                <RecordButton
+                  onClick={SpeechRecognition.stopListening}
+                  style={{ width: 60, height: 60, "margin-left": 70 }}
+                >
+                  {" "}
+                  <i class="animated-icon-stop fa-solid fa-stop fa-2xl"></i>{" "}
+                </RecordButton>
+                <ResetButton
+                  onClick={resetTranscript}
+                  style={{ width: 60, height: 60, "margin-top": 70 }}
+                >
+                  {" "}
+                  <i class="animated-icon-reset fa-solid fa-arrow-rotate-left fa-2xl"></i>{" "}
+                </ResetButton>
+                <ContinuesButton
+                  style={{ width: 60, height: 60, "margin-left": 400 }}
+                >
+                <i class="fa-solid fa-infinity fa-2xl"></i>{" "}
+                </ContinuesButton>
+                <RetractButton
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    setIsOpen(false);
+                  }}
+                  style={{ width: 60, height: 60, "margin-left": 490 }}
+                >
+                  {" "}
+                  <i class="fa-solid fa-minimize fa-2xl"></i>{" "}
+                </RetractButton>
+              </div>
 
               <motion.div className="expand">
-              <TextArea
-                className='textarea'
-                style={{ fontSize: 20, }} spellCheck="false"
-                value={transcript}
-              />
-            </motion.div>
-              </div>
-        
-            ) : (
-              
-              <motion.h2>
-                <h2>Click Here!</h2>
-              </motion.h2>
-            )}
-            
-          </motion.div>
-
-
-  </div>
-
-</div>
-
-);
+                <TextArea
+                  className="textarea"
+                  style={{ fontSize: 20 }}
+                  spellCheck="false"
+                  value={transcript}
+                />
+              </motion.div>
+            </div>
+          ) : (
+            <motion.h2>
+              <h2>Click Here!</h2>
+            </motion.h2>
+          )}
+        </motion.div>
+      </div>
+    </div>
+  );
 };
-
+tippy('#startButton', {
+  content: 'Start Recording',
+});
 
 const Button = styled.button`
-    position: absolute;
-    width: 50px;
-    height: 30px;
-    border-radius: 10px;
-    color: #fff;
-    border: none;
-    cursor: pointer;
+  position: absolute;
+  width: 50px;
+  height: 30px;
+  border-radius: 10px;
+  color: #fff;
+  border: none;
+  cursor: pointer;
 `;
 
 const RecordButton = styled(Button)`
-    background-color: #4d1c1c
+    background-color: grey;
+    transition: background-color 0.3s cubic-bezier(0.25, 0.1, 0.25, 1);
+  &:hover {
+    background-position: 0 0;
+    background-color: rebeccapurple;
+    animation: fa-beat .5s; 
+   i {
+      animation: fa-bounce 1s; 
+    }
+  }
 `;
 
 const PlayButton = styled(Button)`
-    background-color: green
+    background-color: grey;
+    transition: background-color 0.3s cubic-bezier(0.25, 0.1, 0.25, 1);
+  &:hover {
+    background-position: 0 0;
+    background-color: rebeccapurple;
+    animation: fa-beat .5s; 
+   i {
+      animation: fa-bounce 1s; 
+    }
+  }
 `;
 
 const ResetButton = styled(Button)`
-    background-color: grey
-`
+    background-color: grey;
+    transition: background-color 0.3s cubic-bezier(0.25, 0.1, 0.25, 1);
+  &:hover {
+    background-position: 0 0;
+    background-color: rebeccapurple;
+    animation: fa-beat .5s; 
+   i {
+      animation: fa-spin reverse .4s;
+    }
+  }
+`;
 const RetractButton = styled(Button)`
-    background-color: pink
+    background-color: grey;
+    transition: background-color 0.3s cubic-bezier(0.25, 0.1, 0.25, 1);
+  &:hover {
+    background-position: 0 0;
+    background-color: rebeccapurple;
+    animation: fa-beat .5s; 
+   i {
+      animation: fa-bounce 1s; 
+    }
+  }
+`;
+
+const ContinuesButton = styled(Button)`
+    background-color: grey;
+    transition: background-color 0.3s cubic-bezier(0.25, 0.1, 0.25, 1);
+  &:hover {
+    background-position: 0 0;
+    background-color: rebeccapurple;
+    animation: fa-beat .5s; 
+   i {
+      animation: fa-bounce 1s; 
+    }
+  }
 `;
 
 // https://www.twilio.com/blog/audio-visualisation-web-audio-api--react
-// const AudioVisualizer = styled.button`
-//     position: absolute;
-//     width: 50px;
-//     height: 30px;
-//     background-color: #007bff;
-//     border-radius: 10px;
-//     color: #fff;
-//     border: none;
-//     cursor: pointer;
-// `;
 
-// width: 450px
-// top: 60%
 const BaseRectangle = styled.button`
-    display: flex;
-    justify-content: center;
-    width: 550px;
-    height: 120px;
-    background-color: #2b2b2b;
-    border-radius: 50px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    cursor: pointer;
-    border-color: white;
+  display: flex;
+  justify-content: center;
+  width: 550px;
+  height: 120px;
+  background-color: #2b2b2b;
+  border-radius: 50px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  cursor: pointer;
+  border-color: white;
 `;
 
 const TextArea = styled.textarea`
-    position: absolute;
-    resize: none;
-    top: 60%;
-    transform: translateY(-50%);
-    flex: 0.7;
-    border-radius: 10px;
-    color: white;
-    background-color: black;
-    font-weight: 600;
-    border-color: black;
+  position: absolute;
+  resize: none;
+  top: 60%;
+  transform: translateY(-50%);
+  flex: 0.7;
+  border-radius: 10px;
+  color: white;
+  background-color: black;
+  font-weight: 600;
+  border-color: black;
 `;
-// Load module from Skypack CDN
 
 
 
